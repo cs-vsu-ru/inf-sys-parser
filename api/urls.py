3feeecaf1978ef28770fcf9395157d60cab93f5c
Sergey Volchenko <se.volchenko@ya.ru>
2024-06-03 20:34:50 +0300

    ISAPP-78: убрал префикс

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = []
 
 urlpatterns += [
-    path('inf-sys-parser/api/__docs__/', SpectacularAPIView.as_view(), name='__docs__'),
-    path('inf-sys-parser/api/', SpectacularSwaggerView.as_view(url_name='__docs__')),
-    path('inf-sys-parser/api/lessons/', include('app.lessons.urls')),
-    path('inf-sys-parser/api/employees/', include('app.employees.urls')),
+    path('api/__docs__/', SpectacularAPIView.as_view(), name='__docs__'),
+    path('api/', SpectacularSwaggerView.as_view(url_name='__docs__')),
+    path('api/lessons/', include('app.lessons.urls')),
+    path('api/employees/', include('app.employees.urls')),
     *static(settings.BASE_STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
