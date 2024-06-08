83acaee8b2e36855bdb55600b1e7eefa557e703a
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-06-08 16:10:37 +0300

    fix employees

--- app/employees/services/synker.py
+++ app/employees/services/synker.py
@@ -32,18 +32,15 @@ class EmployeeSynker:
             self._update_or_create_employee(employee_data['id'], name)
 
     def synk_by_id(self, id: int) -> Employee:
-        employees_data = self.get_filtered_employees_data()
-        for employee_data in employees_data:
-            if employee_data['id'] == id:
-                name = self._parse_name(employee_data)
-                is_created, employee = self._update_or_create_employee(id, name)
-                if is_created:
-                    self.lessons_manager.create_for_employee(employee)
-                return employee
-        raise KeyError
+        employee_data = self.requester.get(self.url + f'/{id}').json()
+        name = self._parse_name(employee_data)
+        is_created, employee = self._update_or_create_employee(id, name)
+        if is_created:
+            self.lessons_manager.create_for_employee(employee)
+        return employee
 
     def _update_or_create_employee(
-        self, id: int, name: str
+            self, id: int, name: str
     ) -> tuple[IsCreated, Employee]:
         try:
             employee = self.employee_manager.get(id=id)
@@ -61,10 +58,7 @@ class EmployeeSynker:
         return f"{last_name} {first_name[0]}.{patronymic[0]}."
 
     def delete_lessons_by_id(self, id: int):
-        employees_data = self.requester.get(self.url).json()
-        for employee_data in employees_data:
-            if employee_data['id'] == id:
-                employee_to_delete = self.employee_manager.get(id=employee_data['id'])
-                self.lessons_manager.delete_for_employee(employee_to_delete)
-                employee_to_delete.delete()
-        raise KeyError
+        employee_to_delete = self.employee_manager.get(id=id)
+        self.lessons_manager.delete_for_employee(employee_to_delete)
+        employee_to_delete.delete()
+    raise KeyError
